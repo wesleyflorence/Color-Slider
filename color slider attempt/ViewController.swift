0ebb9091c3d2c03a7a7120533f863341e7b0b2cf
wesleyflorence <jw37g@mac.com>
2017-06-23 18:57:53 -0400

    Initial Commit

---
+++ color slider attempt/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  color slider attempt
+//
+//  Created by Wesley Florence on 6/23/17.
+//  Copyright © 2017 Wesley Florence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+    
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: Actions
+
+    @IBAction func changeColorComponent(_ sender: UISlider) {
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
